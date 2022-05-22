9cb84847ba91db0b2e25339f69855716d9f6cb70
swxtz <82668127+swxtz@users.noreply.github.com>
2022-05-21 21:10:51 -0300

    backend

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
-
-const teste =1;
+import cors from 'cors'
+import { routes } from './routes';
 
 const app = express(); 
 
-app.listen(3333,
+app.use(cors());
+app.use(express.json());
+app.use(routes)
+
+app.listen(3333, () => {
+    console.log('HTTP server running!!');
+});
